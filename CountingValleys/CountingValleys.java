6c79d547f576533e11fd63c6ce08bd8c379982ef
Ricardo Mendes <ricardominamendes@outlook.com>
2023-02-08 15:36:56 +0000

    Finished CountingValleys exercise

---
+++ CountingValleys/CountingValleys.java
@@ -0,0 +1,68 @@
+public class CountingValleys {
+
+    public static int numberOfValleys(String stepsString){
+
+        int level = 0;
+
+        int bellowSeaLevelConsecutiveStepsCounter = 0;
+
+        int valleyCounter = 0;
+
+        boolean isValley = false;
+
+        boolean isOutsideValley = true;
+
+        for (int i = 0; i < stepsString.length(); i++) {
+
+            if(stepsString.charAt(i) == 'U'){
+                level++;
+                bellowSeaLevelConsecutiveStepsCounter--;
+            }
+            
+            if(stepsString.charAt(i) == 'D'){
+                level--;
+                bellowSeaLevelConsecutiveStepsCounter++;
+            }
+
+            if(bellowSeaLevelConsecutiveStepsCounter == 2){
+                isValley = true;
+            }
+
+            if(bellowSeaLevelConsecutiveStepsCounter != 2){
+                isValley = false;
+            }
+
+            if(isValley && isOutsideValley){
+                valleyCounter++;
+                isOutsideValley = false;
+            }
+
+            if(level == 0){
+                isOutsideValley = true;
+            }
+
+        }
+
+        return valleyCounter;
+
+    }
+
+    public static void main(String[] args) {
+
+        String samplePattern = "UDDDUDUU";
+        String samplePattern2 = "UUDUDUDDUDUDUDDDDDDUDUU";
+        String samplePattern3 = "UDDDUDDUUUUDUDUDUUUDUDDUU";
+        String samplePattern4 = "UDDDUDUUDUDUDUDDDUUUUDDUU";
+        String twoValleysOneMountain = "UDDDUDUUUUUDDDDDDDDUUUDUDUDDUUUU";
+        String twoMountainsOneValley = "UUDDDDUDUUUUDDUUDD";
+
+        
+        //System.out.println("\n" + numberOfValleys(samplePattern));
+        //System.out.println("\n" + numberOfValleys(samplePattern2));
+        //System.out.println("\n" + numberOfValleys(samplePattern3));
+        //System.out.println("\n" + numberOfValleys(samplePattern4));
+        //System.out.println("\n" + numberOfValleys(twoValleysOneMountain));
+        System.out.println("\n" + numberOfValleys(twoMountainsOneValley));
+    }
+    
+}
