56c0af036d1e75b507ca31ee87f8f6f62d3b7a7c
Ricardo Mendes <ricardominamendes@outlook.com>
2023-02-09 18:33:20 +0000

    added some comments for clarity

--- RepeatedString/RepeatedString.java
+++ RepeatedString/RepeatedString.java
@@ -4,6 +4,8 @@ public class RepeatedString {
 
     public static long repeatedStringMethod(String string, double n){
         
+
+        // Let's count how many 'a' the string has
         double counter = 0;
 
         for (char letter : string.toCharArray()) {
@@ -11,6 +13,12 @@ public class RepeatedString {
                 counter++;
         }
         
+        /* 
+         * 'n' divided by the string length will tell us how many times the word can fit in 'n'
+         * The Math.round will always be exact because if result < .49 it means the word would not fit and if result > .50 then it will fit
+         * When we multiply by 'counter' we know exactly how many 'a' we have 
+         */
+
         return Math.round((n / string.length()) * counter ); 
     }
 
