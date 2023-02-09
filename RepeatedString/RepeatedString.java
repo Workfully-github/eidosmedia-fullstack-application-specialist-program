21d8a3bcac87380194e3526bba175b4ae1811178
Ricardo Mendes <ricardominamendes@outlook.com>
2023-02-09 18:03:54 +0000

    archived old idea, and came up with a new one to solve this exercise

--- RepeatedString/RepeatedString.java
+++ RepeatedString/RepeatedString.java
@@ -2,43 +2,23 @@ package RepeatedString;
 
 public class RepeatedString {
 
-    public static int repeatedStringMethod(String string, int n){
-
-        int counter = 0;
-
-        String repeatedString = concatenator(string, n);   
+    public static long repeatedStringMethod(String string, double n){
         
-        for(int i = 0; i < repeatedString.length(); i++){
+        double counter = 0;
 
-            if(repeatedString.charAt(i) == 'a')
+        for (char letter : string.toCharArray()) {
+            if(letter == 'a')
                 counter++;
         }
-
-        return counter;
-    }
-
-    private static String concatenator(String string, int n){
-
-        String concatenatedString = "";
-
-        //Without the power of Streams will take forever to calculate
-        /*while(concatenatedString.length() < n)
-            concatenatedString += string;
-
-        return concatenatedString.substring(0, n);*/
-
-        //With the power of Streams it's much faster to calculate
-        return concatenatedString
-            .concat(string)
-            .repeat(n)
-            .substring(0, n);
+        
+        return Math.round((n / string.length()) * counter ); 
     }
 
     public static void main(String[] args) {
 
-        System.out.println(repeatedStringMethod("aba", 10));
+        System.out.println(repeatedStringMethod("aba", 10d));
 
-        System.out.println(repeatedStringMethod("a", 1000000000));
+        System.out.println(repeatedStringMethod("a", 1000000000000d)); 
 
     }
     
---
+++ RepeatedString/RepeatedStringsArchivedIdea.java
@@ -0,0 +1,26 @@
+package RepeatedString;
+
+public class RepeatedStringsArchivedIdea {
+
+    private static String concatenatorForInts(String string, int n){
+
+        String concatenatedString = "";
+
+        //Without the power of Streams will take forever to calculate
+/*         while(concatenatedString.length() < n){
+            concatenatedString += string;
+            System.out.println(concatenatedString.length());
+        }
+
+        return concatenatedString; */
+
+
+        //With the power of Streams it's much faster to calculate but only works with int
+        return concatenatedString
+            .concat(string)
+            .repeat(n)
+            .substring(0, n);
+    }
+
+    
+}
