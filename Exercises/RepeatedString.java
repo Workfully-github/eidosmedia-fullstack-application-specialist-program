54574cd2456a833423321f076a00c4973f5cf833
Hugo <hugo.veigaa@gmail.com>
2023-02-09 17:24:39 +0000

    solving the Repeated String exercise

--- Exercises/RepeatedString.java
+++ Exercises/RepeatedString.java
@@ -1,17 +1,34 @@
 public class RepeatedString {
     public static void main (String[] args) {
-
         int numberA = 0;
-        int n = 10;
-        String letters = "aba";
+        int n = 100;
+        String letters = "ricardo";
+        int numbLetters = letters.length();
+        int restOfLetters = n - numbLetters;
+        int letterNotCounted = restOfLetters % numbLetters;
 
-
+        // First let's count the number of times that a's appears in the string
         for (int i = 0; i < letters.length(); i++) {
             if (letters.charAt(i) == 'a') {
                 numberA ++;
             }
         }
 
+        // Now we can count the number of times that the whole string is repeated.
+        // So we sum to the number of a's the number of times that the word is repeated times the number
+        // of a's in the initial string.
+        numberA = (restOfLetters / numbLetters) * numberA + numberA;
+
+        // To finish we just need to take into account the letters that are not enough to
+        // complete the whole string.
+        // So we know that the number o missing letter is smaller than the lenght of the initial string
+        // This means that we only need to count the number of a's in a string smaller than our inicial
+        // string and add it to the number of a's.
+        for (int j = 0; j < letterNotCounted; j++) {
+            if (letters.charAt(j) == 'a') {
+                numberA ++;
+            }
+        }
         System.out.println(numberA);
     }
 }
