e45aeac872c8fe338c72e2e69a4733c82e14cf92
Hugo <hugo.veigaa@gmail.com>
2023-02-09 15:57:58 +0000

    first draft of RepeatedString exercise

---
+++ Exercises/9.02.23/RepeatedString.java
@@ -0,0 +1,16 @@
+public class RepeatedString {
+    public static void main (String[] args) {
+
+        int numberA = 0;
+        int n = 10;
+        String letters = "aba";
+
+        for (int i = 0; i < letters.length; i++) {
+            if (letters.charAt(i) == 'a') {
+                numberA ++;
+            }
+        }
+
+        System.out.println(numberA);
+    }
+}
--- Training/9.02.23/SchoolReport.java
+++ Training/9.02.23/SchoolReport.java
@@ -1,5 +1,4 @@
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List; 
 
@@ -17,52 +16,62 @@ public class SchoolReport {
         HashMap<String, List<Integer>> notesMonth2 = new HashMap<>();
 
         List<Integer> notesJan = new ArrayList<>();
-
         notesJan.add(15);
         notesJan.add(16);
         notesJan.add(13);
         notesMonth2.put("January", notesJan);
+
         List<Integer> notesFeb = new ArrayList<>();
         notesFeb.add(14);
         notesFeb.add(18);
         notesMonth2.put("February", notesFeb);
+
         List<Integer> notesMar = new ArrayList<>();
         notesMar.add(16);
         notesMonth2.put("March", notesMar);
+
         List<Integer> notesApr = new ArrayList<>();
         notesApr.add(11);
         notesApr.add(20);
         notesApr.add(9);
         notesMonth2.put("April", notesApr);
+
         List<Integer> notesMay = new ArrayList<>();
         notesMay.add(20);
         notesMonth2.put("May", notesMay);
+
         List<Integer> notesJun = new ArrayList<>();
         notesJun.add(13);
         notesJun.add(19);
         notesMonth2.put("June", notesJun);
+
         List<Integer> notesJul = new ArrayList<>();
         notesJul.add(11);
         notesJul.add(15);
         notesJul.add(7);
         notesMonth2.put("July", notesJul);
+
         List<Integer> notesAug = new ArrayList<>();
         notesAug.add(14);
         notesAug.add(13);
         notesAug.add(15);
         notesMonth2.put("August", notesAug);
+
         List<Integer> notesSep = new ArrayList<>();
         notesSep.add(11);
         notesSep.add(20);
         notesSep.add(9);
         notesMonth2.put("September", notesSep);
+
         List<Integer> notesOct = new ArrayList<>();
         notesOct.add(11);
         notesMonth2.put("October", notesOct);
+
         List<Integer> notesNov = new ArrayList<>();
         notesNov.add(11);
         notesNov.add(20);
         notesMonth2.put("November", notesNov);
+
         List<Integer> notesDec = new ArrayList<>();
         notesDec.add(20);
         notesDec.add(9);
@@ -70,30 +79,6 @@ public class SchoolReport {
 
         System.out.println(notesMonth2);
 
-
-
-        /* ArrayList<Integer> notes = new ArrayList<Integer>();
-
-        notes.add(15);
-        notes.add(15);
-        notes.add(15);
-        notes.add(15);
-        notes.add(15);
-        notes.add(17);
-        notes.add(17);
-        notes.add(17);
-        notes.add(17);
-        notes.add(17);
-        notes.add(18);
-        notes.add(18);
-        notes.add(18);
-        notes.add(18);
-        notes.add(18);
-        notes.add(20);
-        notes.add(20);
-        notes.add(20);
-        notes.add(20);
-        notes.add(20);
 
         int sum = 0;
 
