202e76a9e941f3d13130a3078fc27a696d525b86
Ricardo Mendes <ricardominamendes@outlook.com>
2023-02-09 10:31:09 +0000

    Finished MostExpensive exercise

---
+++ MostExpensive/MostExpensive.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+
+public class MostExpensive {
+
+    public static void main(String[] args) {
+        
+        ArrayList<Double> decimalNumbers = new ArrayList<>(){
+            {
+                add(74.46);
+                add(63.99);
+                add(10.57);
+                add(81.37);
+            }
+        };
+
+        double mostExpensive = 0;
+
+        for (Double decimalNumber : decimalNumbers) {
+
+            if(decimalNumber > mostExpensive){
+                mostExpensive = decimalNumber;
+            }
+        }
+
+        System.out.println("Most Expensive: " + mostExpensive);
+
+    }
+}
