1931b9657739bee1992aa6b5443dd35042777a6e
Hugo <hugo.veigaa@gmail.com>
2023-02-15 15:02:09 +0000

    new approach using code from class

--- Exercises/Soccer_Match/SoccerFinalGame.java
+++ Exercises/Soccer_Match/SoccerFinalGame.java
@@ -8,32 +8,51 @@ public class SoccerFinalGame {
     public static void main(String[] args) {
 
         SoccerLeague englandCup;
-        SoccerMatch cityVsLiverpool;
-        SoccerMatch unitedVsNewcastle;
-        SoccerTeam manchesterCity;
-        SoccerTeam liverpoolFC;
-        SoccerTeam manchesterUnited;
-        SoccerTeam newcastleFC;
+        SoccerMatch cityVsLiverpool, unitedVsNewcastle, cityVsNewcastle, unitedVsLiverpool;
+        SoccerTeam manchesterCity, liverpoolFC, manchesterUnited, newcastleFC;
 
         ArrayList<SoccerTeam> oponnentsGame1 = new ArrayList<SoccerTeam>();
         ArrayList<SoccerTeam> oponnentsGame2 = new ArrayList<SoccerTeam>();
+        ArrayList<SoccerTeam> oponnentsGame3 = new ArrayList<SoccerTeam>();
+        ArrayList<SoccerTeam> oponnentsGame4 = new ArrayList<SoccerTeam>();
         ArrayList<SoccerTeam> leagueTeams = new ArrayList<SoccerTeam>();
         ArrayList<SoccerPlayer> city = new ArrayList<SoccerPlayer>();
         ArrayList<SoccerPlayer> liverpool = new ArrayList<SoccerPlayer>();
         ArrayList<SoccerPlayer> united = new ArrayList<SoccerPlayer>();
         ArrayList<SoccerPlayer> newcastle = new ArrayList<SoccerPlayer>();
         ArrayList<SoccerMatch> allMatches = new ArrayList<SoccerMatch>();
-
-        HashMap<SoccerTeam, Integer> points = new HashMap<SoccerTeam, Integer>();
-        HashMap<SoccerTeam, Integer> wins = new HashMap<SoccerTeam, Integer>();
-        HashMap<SoccerTeam, Integer> losses = new HashMap<SoccerTeam, Integer>();
-        HashMap<SoccerTeam, Integer> draws = new HashMap<SoccerTeam, Integer>();
-        HashMap<SoccerTeam, Integer> goalsScored = new HashMap<SoccerTeam, Integer>();
-        HashMap<SoccerTeam, Integer> goalsSuffered = new HashMap<SoccerTeam, Integer>();
+       
+        // Creeate Maps 
+        HashMap<SoccerTeam, Integer> points = new HashMap<>() {
+            {for (int i = 0; i < leagueTeams.size(); i ++ ) {
+                    put(leagueTeams.get(i), 0);
+                }}};
+        HashMap<SoccerTeam, Integer> wins = new HashMap<SoccerTeam, Integer>() {
+            {for (int i = 0; i < leagueTeams.size(); i ++ ) {
+                    put(leagueTeams.get(i), 0);
+                }}};
+        HashMap<SoccerTeam, Integer> losses = new HashMap<SoccerTeam, Integer>() {
+            {for (int i = 0; i < leagueTeams.size(); i ++ ) {
+                    put(leagueTeams.get(i), 0);
+                }}};
+        HashMap<SoccerTeam, Integer> draws = new HashMap<SoccerTeam, Integer>() {
+            {for (int i = 0; i < leagueTeams.size(); i ++ ) {
+                    put(leagueTeams.get(i), 0);
+                }}};
+        HashMap<SoccerTeam, Integer> goalsScored = new HashMap<SoccerTeam, Integer>() {
+            {for (int i = 0; i < leagueTeams.size(); i ++ ) {
+                    put(leagueTeams.get(i), 0);
+                }}};
+        HashMap<SoccerTeam, Integer> goalsSuffered = new HashMap<SoccerTeam, Integer>() {
+            {for (int i = 0; i < leagueTeams.size(); i ++ ) {
+                    put(leagueTeams.get(i), 0);
+                }}};
 
         // Create matches
         cityVsLiverpool = new SoccerMatch(oponnentsGame1, "Wembley Stadium", "Soares Dias");
         unitedVsNewcastle = new SoccerMatch(oponnentsGame2, "Dragão", "Collina");
+        cityVsNewcastle = new SoccerMatch(oponnentsGame3, "Dragão", "Collina");
+        unitedVsLiverpool = new SoccerMatch(oponnentsGame4, "Wembley Stadium", "Soares Dias");
 
         // Create players
         setCityPlayers(city);
@@ -43,42 +62,59 @@ public class SoccerFinalGame {
 
         // Create League
         englandCup = new SoccerLeague("England Cup", leagueTeams, allMatches, points, wins, losses, draws, goalsScored, goalsSuffered);
-        // Need to create the missing fields to complete league
 
         // Create teams
         manchesterCity = getHomeTeam("Manchester City", city, "Guardiola", "Manchester");
         liverpoolFC = getAwayTeam("Liverpool", liverpool, "Klopp", "Liverpool");
         manchesterUnited = getAwayTeam("Liverpool", united, "Klopp", "Liverpool");
         newcastleFC = getAwayTeam("Newcastle", newcastle, "Howe", "Newcastle");
-
-        // Create goals
-        SoccerGoal goalCity1 = scoreGoal(manchesterCity.getPlayers().get(6), 12, manchesterCity.getPlayers().get(6), manchesterCity);
-        SoccerGoal goalCity2 = scoreGoal(manchesterCity.getPlayers().get(1), 25, manchesterCity.getPlayers().get(8), manchesterCity);
-        SoccerGoal goalLiverpool1 = scoreGoal(liverpoolFC.getPlayers().get(3), 44, liverpoolFC.getPlayers().get(5), liverpoolFC);
-        SoccerGoal goalLiverpool2 = scoreGoal(liverpoolFC.getPlayers().get(2), 56, liverpoolFC);
         
-        // Score goals
-        cityVsLiverpool.scoreAGoal(goalCity1);
-        cityVsLiverpool.scoreAGoal(goalCity2);
-        cityVsLiverpool.scoreAGoal(goalLiverpool1);
-        cityVsLiverpool.scoreAGoal(goalLiverpool2);
-
         // Create oponnents array
         oponnentsGame1.add(manchesterCity);
         oponnentsGame1.add(liverpoolFC);
         
         oponnentsGame2.add(manchesterUnited);
         oponnentsGame2.add(newcastleFC);
-
+        
+        oponnentsGame3.add(manchesterCity);
+        oponnentsGame3.add(newcastleFC);
+        
+        oponnentsGame4.add(newcastleFC);
+        oponnentsGame4.add(liverpoolFC);
+        
         // Create array with teams in league
         leagueTeams.add(manchesterCity);
         leagueTeams.add(liverpoolFC);
         leagueTeams.add(manchesterUnited);
         leagueTeams.add(newcastleFC);
-
+        
         // Create array with league matches
         allMatches.add(cityVsLiverpool);
         allMatches.add(unitedVsNewcastle);
+        allMatches.add(cityVsNewcastle);
+        allMatches.add(unitedVsLiverpool);
+
+        // Create goals cityVsLiverpool
+        SoccerGoal goalCity1 = scoreGoal(manchesterCity.getPlayers().get(6), 12, manchesterCity.getPlayers().get(6), manchesterCity);
+        SoccerGoal goalCity2 = scoreGoal(manchesterCity.getPlayers().get(1), 25, manchesterCity.getPlayers().get(8), manchesterCity);
+        SoccerGoal goalLiverpool1 = scoreGoal(liverpoolFC.getPlayers().get(3), 44, liverpoolFC.getPlayers().get(5), liverpoolFC);
+        SoccerGoal goalLiverpool2 = scoreGoal(liverpoolFC.getPlayers().get(2), 56, liverpoolFC);
+        
+        // Score goals cityVsLiverpool
+        cityVsLiverpool.scoreAGoal(goalCity1);
+        cityVsLiverpool.scoreAGoal(goalCity2);
+        cityVsLiverpool.scoreAGoal(goalLiverpool1);
+        cityVsLiverpool.scoreAGoal(goalLiverpool2);
+    }
+
+    private void checkWinner(ArrayList<SoccerGoal> goals, ArrayList<SoccerTeam> game) {
+        int numGoalsHomeTeam = 0;
+        int numGoalsAwayTeam = 0;
+        for(int i = 0; i < goals.size(); i ++) {
+            if (goals.get(i).getTeam().getTeamName() == game.get(0).getTeamName())  {
+
+            }
+        }
     }
 
     private static SoccerTeam getHomeTeam(String name, ArrayList<SoccerPlayer> players, String coach, String city) {
@@ -112,7 +148,6 @@ public class SoccerFinalGame {
     }
 
     private static ArrayList<SoccerPlayer> setCityPlayers(ArrayList<SoccerPlayer> city) {
-        //ArrayList<SoccerPlayer> homeTeamPlayers = new ArrayList<SoccerPlayer>();
         for (int i = 1; i <= 11; i++) {
             city.add(getHomeTeamPlayers("Player " + i, i, "Some", 0, 0));
         }
@@ -120,7 +155,6 @@ public class SoccerFinalGame {
     }
 
     private static ArrayList<SoccerPlayer> setLiverpoolPlayers(ArrayList<SoccerPlayer> liverpool) {
-        //ArrayList<SoccerPlayer> homeTeamPlayers = new ArrayList<SoccerPlayer>();
         for (int i = 12; i <= 22; i++) {
             liverpool.add(getAwayTeamPlayers("Player " + i, i, "Some", 0, 0));
         }
@@ -128,7 +162,6 @@ public class SoccerFinalGame {
     }
 
     private static ArrayList<SoccerPlayer> setUnitedPlayers(ArrayList<SoccerPlayer> united) {
-        //ArrayList<SoccerPlayer> homeTeamPlayers = new ArrayList<SoccerPlayer>();
         for (int i = 23; i <= 33; i++) {
             united.add(getAwayTeamPlayers("Player " + i, i, "Some", 0, 0));
         }
@@ -136,23 +169,9 @@ public class SoccerFinalGame {
     }
 
     private static ArrayList<SoccerPlayer> setNewcastlePlayers(ArrayList<SoccerPlayer> newcastle) {
-        //ArrayList<SoccerPlayer> homeTeamPlayers = new ArrayList<SoccerPlayer>();
         for (int i = 23; i <= 33; i++) {
             newcastle.add(getAwayTeamPlayers("Player " + i, i, "Some", 0, 0));
         }
         return newcastle;
     }
 }
-
-        /* SoccerPlayer player1 = getHomeTeamPlayers("Player 1", 1, "Some", 0, 0);
-        SoccerPlayer player2 = getHomeTeamPlayers("Player 1", 1, "Some", 0, 0);
-        SoccerPlayer player1 = getHomeTeamPlayers("Player 1", 1, "Some", 0, 0);
-        SoccerPlayer player1 = getHomeTeamPlayers("Player 1", 1, "Some", 0, 0);
-        SoccerPlayer player1 = getHomeTeamPlayers("Player 1", 1, "Some", 0, 0);
-        SoccerPlayer player1 = getHomeTeamPlayers("Player 1", 1, "Some", 0, 0);
-        SoccerPlayer player1 = getHomeTeamPlayers("Player 1", 1, "Some", 0, 0);
-        SoccerPlayer player1 = getHomeTeamPlayers("Player 1", 1, "Some", 0, 0);
-        SoccerPlayer player1 = getHomeTeamPlayers("Player 1", 1, "Some", 0, 0);
-        SoccerPlayer player1 = getHomeTeamPlayers("Player 1", 1, "Some", 0, 0);
-        SoccerPlayer player1 = getHomeTeamPlayers("Player 1", 1, "Some", 0, 0);
-        SoccerPlayer player1 = getHomeTeamPlayers("Player 1", 1, "Some", 0, 0); */
--- Exercises/Soccer_Match/SoccerLeague.java
+++ Exercises/Soccer_Match/SoccerLeague.java
@@ -8,26 +8,80 @@ public class SoccerLeague {
     private String name;
     private ArrayList<SoccerTeam> teams;
     private ArrayList<SoccerMatch> matches;
-    private HashMap<SoccerTeam, Integer> points;
-    private HashMap<SoccerTeam, Integer> wins;
+    private HashMap<SoccerTeam, Integer> standings = new HashMap<SoccerTeam,Integer>();
+    /* private HashMap<SoccerTeam, Integer> wins;
     private HashMap<SoccerTeam, Integer> losses;
     private HashMap<SoccerTeam, Integer> draws;
     private HashMap<SoccerTeam, Integer> goalsScored;
-    private HashMap<SoccerTeam, Integer> goalsSuffered;
+    private HashMap<SoccerTeam, Integer> goalsSuffered; */
+
+    private static int POINTS_WIN = 3;
+    private static int POINTS_DRAW = 1;
+    private static int POINTS_DEFEAT = 0;
 
     public SoccerLeague(String name, ArrayList<SoccerTeam> teams, ArrayList<SoccerMatch> matches,
-    HashMap<SoccerTeam, Integer> points, HashMap<SoccerTeam, Integer> wins, HashMap<SoccerTeam,
+    HashMap<SoccerTeam, Integer> standings)
+    /*  HashMap<SoccerTeam, Integer> wins, HashMap<SoccerTeam,
     Integer> losses, HashMap<SoccerTeam, Integer> draws, HashMap<SoccerTeam, Integer> goalsScored,
-    HashMap<SoccerTeam, Integer> goalsSuffered) {
+    HashMap<SoccerTeam, Integer> goalsSuffered) */ {
         this.name = name;
         this.teams = teams;
-        this.matches = matches;
-        this.points = points;
+        generateStandings();
+        /* this.matches = matches;
+        this.standings = standings;
         this.wins = wins;
         this.losses = losses;
         this.draws = draws;
         this.goalsScored = goalsScored;
-        this.goalsSuffered = goalsSuffered;
+        this.goalsSuffered = goalsSuffered; */
+    }
+
+    public void setMatches(ArrayList<SoccerMatch> matches) {
+        this.matches = matches;
+    }
+
+    public void playMatches() {
+        // generate result of all league matches
+        // assignPoints(match);
+    }
+
+    private void assignPoints(SoccerMatch match){
+        HashMap<SoccerTeam,Integer> result = match.getResult();
+        
+        if(match.isADraw()){
+            int previousPoints = standings.get(result.get(0));
+            int newPoints = previousPoints += POINTS_DRAW;
+            standings.put(result.get(0), newPoints);
+            standings.put(result.get(1), newPoints);
+        }
+
+        //assign points to the winner        
+        SoccerTeam winner = match.getWinner();
+
+        int previousPoints = standings.get(winner);
+        int newPoints = previousPoints += POINTS_WIN;
+        standings.put(winner, newPoints);
+
+
+
+
+
+        // assign points to the teams based on match result 
+    }
+
+    private void generateStandings(){
+        //TODO update the hashmap based on the teams
+        // use alphabetical sorting
+
+    }
+
+    /**
+     * @return the standings
+     */
+    public HashMap<FootballTeam, Integer> getStandings() {
+
+        //TODO sort the standings based on value
+        return standings;
     }
 
     public HashMap<SoccerTeam, Integer> getPoints() {
