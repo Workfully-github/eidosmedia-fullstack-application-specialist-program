c0b40a6cc7b59da60abc5fa73312ebe42d3ecb61
Ricardo Mendes <ricardominamendes@outlook.com>
2023-02-16 21:17:55 +0000

    change in class HomeFeed

--- src/org/workfully/section/HomeFeed.java
+++ src/org/workfully/section/HomeFeed.java
@@ -4,11 +4,11 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 import org.workfully.content.AbstractContent;
-import org.workfully.users.Author;
+import org.workfully.users.AbstractUser;
 
 public class HomeFeed extends AbstractSection {
 
     /* PROPERTIES */
-    private HashMap<Author, ArrayList<AbstractContent>> globalPostLogMap;
+    private HashMap<AbstractUser, ArrayList<AbstractContent>> globalPostLogMap;
     
 }
