8ccbfe05fbad71b588fd58b86507423d4a50d491
Sidi TOURÉ <37058375+sidi001@users.noreply.github.com>
2023-03-01 10:28:13 +0100

    home feed with http get json

--- Social Media News jsondata/src/Main.java
+++ Social Media News jsondata/src/Main.java
@@ -8,76 +8,22 @@ import Models.User.Author;
 import Models.User.BasicUser;
 import Models.User.PremuimUser;
 import View.InitPost_json;
-import View.InitUser;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-        String json ="{\"feed\": {\n" +
-                "  \"tweets\": [\n" +
-                "    {\n" +
-                "      \"title\": \"tweet for today\",\n" +
-                "      \"text\": \"we just receive 100 dollars order from our platform\",\n" +
-                "      \"author\": {\n" +
-                "        \"name\": \"John Doe\",\n" +
-                "        \"id\": \"1\",\n" +
-                "        \"type\": \"basic\"\n" +
-                "      },\n" +
-                "      \"likes\": [],\n" +
-                "      \"comments\": [],\n" +
-                "      \"premium\": true\n" +
-                "    },\n" +
-                "    {\n" +
-                "      \"title\": \"My Second Post\",\n" +
-                "      \"text\": \"Goodbye World!\",\n" +
-                "      \"author\": {\n" +
-                "        \"name\": \"John Doe\",\n" +
-                "        \"id\": \"1\",\n" +
-                "        \"type\": \"basic\"\n" +
-                "      },\n" +
-                "      \"likes\": [],\n" +
-                "      \"comments\": [],\n" +
-                "      \"premium\": false\n" +
-                "    },\n" +
-                "    {\n" +
-                "      \"title\": \"a other Post\",\n" +
-                "      \"text\": \"Hello World!\",\n" +
-                "      \"author\": {\n" +
-                "        \"name\": \"Bob\",\n" +
-                "        \"id\": \"2\",\n" +
-                "        \"type\": \"premium\"\n" +
-                "      },\n" +
-                "      \"likes\": [{\n" +
-                "        \"name\": \"John Doe\",\n" +
-                "        \"id\": \"1\",\n" +
-                "        \"type\": \"basic\",\n" +
-                "        \"author\": true\n" +
-                "      }],\n" +
-                "      \"comments\": [\n" +
-                "        {\n" +
-                "            \"user\":{\n" +
-                "                \"name\": \"patrick\",\n" +
-                "                \"id\": \"4\",\n" +
-                "                \"type\": \"premium\",\n" +
-                "                \"author\": false\n" +
-                "              },\n" +
-                "          \"text\": \"best post of the year\"\n" +
-                "        }\n" +
-                "      ],\n" +
-                "      \"premium\": false\n" +
-                "    }\n" +
-                "  ],\n" +
-                "  \"videos\": [],\n" +
-                "  \"texts\": [],\n" +
-                "  \"images\": []\n" +
-                "}\n" +
-                "}\n";
+    public static void main(String[] args) throws IOException {
+        System.out.println("Home feed!");
 
+        String url = "https://mocki.io/v1/6d2abb84-e28a-491e-b175-f04f299ed29f";
+        String method = "GET";
+
+        //read data
+        String httpBodyJson = InitPost_json.getJsonData(url, method);
         //Declare Contents
         List<TweetPost> tweetPostsList = new ArrayList();
         List<VideoPost> videoPostsList = new ArrayList();
@@ -91,7 +37,7 @@ public class Main {
         TweetPost tweetPostVar;
 
         //read json
-        JSONObject jsonObject = new JSONObject(json);
+        JSONObject jsonObject = new JSONObject(httpBodyJson);
         JSONObject jsonFeed = jsonObject.getJSONObject("feed");
 
 
--- Social Media News jsondata/src/View/InitPost_json.java
+++ Social Media News jsondata/src/View/InitPost_json.java
@@ -7,6 +7,13 @@ import Models.User.PremuimUser;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class InitPost_json {
 
     public static TweetPost createTweetPost(JSONObject jsonObjectTweet){
@@ -60,6 +67,40 @@ public class InitPost_json {
         }
 
         return tweetPostVar;
+
+    }
+
+    public static String getJsonData(String url, String method) throws IOException {
+
+        HttpURLConnection connection =  (HttpURLConnection) new URL(url).openConnection();
+        connection.setRequestMethod(method.toUpperCase());
+        connection.setDoOutput(true);
+
+        int responseCode;
+        try {
+            // Get Response Code and Headers
+            responseCode = connection.getResponseCode();
+
+            String responseMessage = connection.getResponseMessage();
+            System.out.println("------------------------------------------------------------------");
+            System.out.println("\nResponse Code: " + responseCode + " " + responseMessage);
+            System.out.println("------------------------------------------------------------------");
+            System.out.println("\nResponse Headers: " + connection.getHeaderFields());
+
+            // Get Response Body (HTML)
+            BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            String inputLine;
+            StringBuilder responseBody = new StringBuilder();
+            while ((inputLine = in.readLine()) != null) {
+                responseBody.append(inputLine);
+            }
+            System.out.println("\nResponse Body: " + responseBody.toString());
+            return responseBody.toString();
+        }
+        catch (Exception e){
+            System.out.println("Http request error");
+            return "{}";
+        }
 
     }
 
