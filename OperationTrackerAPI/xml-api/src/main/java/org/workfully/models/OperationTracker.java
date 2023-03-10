2ba6b212b5a9c0c20b334d76c9cbe9324eded588
Ricardo Mendes <ricardominamendes@outlook.com>
2023-03-10 21:31:57 +0000

    Work in progress: Implementing Model

---
+++ OperationTrackerAPI/xml-api/src/main/java/org/workfully/models/OperationTracker.java
@@ -0,0 +1,74 @@
+// package org.workfully.models;
+
+
+// public class OperationTracker {
+
+//     private int pageRequests;
+//     private int allProductsRequests;
+//     private int productDetailsRequests;
+//     private int searchRequests;
+//     private int searchByCategoryRequests;
+//     private int categoriesRequests;
+       
+//     public OperationTracker() {
+//     }
+
+//     public OperationTracker(int pageRequests, int allProductsRequests, int productDetailsRequests, int searchRequests,
+//             int searchByCategoryRequests, int categoriesRequests) {
+//         this.pageRequests = pageRequests;
+//         this.allProductsRequests = allProductsRequests;
+//         this.productDetailsRequests = productDetailsRequests;
+//         this.searchRequests = searchRequests;
+//         this.searchByCategoryRequests = searchByCategoryRequests;
+//         this.categoriesRequests = categoriesRequests;
+//     }
+
+//     public int getPageRequests() {
+//         return pageRequests;
+//     }
+
+//     public void setPageRequests(int pageRequests) {
+//         this.pageRequests = pageRequests;
+//     }
+
+//     public int getAllProductsRequests() {
+//         return allProductsRequests;
+//     }
+
+//     public void setAllProductsRequests(int allProductsRequests) {
+//         this.allProductsRequests = allProductsRequests;
+//     }
+
+//     public int getProductDetailsRequests() {
+//         return productDetailsRequests;
+//     }
+
+//     public void setProductDetailsRequests(int productDetailsRequests) {
+//         this.productDetailsRequests = productDetailsRequests;
+//     }
+
+//     public int getSearchRequests() {
+//         return searchRequests;
+//     }
+
+//     public void setSearchRequests(int searchRequests) {
+//         this.searchRequests = searchRequests;
+//     }
+
+//     public int getSearchByCategoryRequests() {
+//         return searchByCategoryRequests;
+//     }
+
+//     public void setSearchByCategoryRequests(int searchByCategoryRequests) {
+//         this.searchByCategoryRequests = searchByCategoryRequests;
+//     }
+
+//     public int getCategoriesRequests() {
+//         return categoriesRequests;
+//     }
+
+//     public void setCategoriesRequests(int categoriesRequests) {
+//         this.categoriesRequests = categoriesRequests;
+//     }
+    
+// }
