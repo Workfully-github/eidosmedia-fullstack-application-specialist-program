110ffcf3cf3964166773eebd4db389d44e535843
Ricardo Mendes <ricardominamendes@outlook.com>
2023-03-10 13:22:47 +0000

    feat: Encapsulation Improve on method getProductsCounter(), added log information if request successeful/fails

--- OperationTrackerAPI/xml-api/src/main/java/org/workfully/http/GetStats.java
+++ OperationTrackerAPI/xml-api/src/main/java/org/workfully/http/GetStats.java
@@ -1,7 +1,6 @@
 package org.workfully.http;
 
 import java.io.StringWriter;
-
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -11,7 +10,6 @@ import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
-
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.w3c.dom.Document;
