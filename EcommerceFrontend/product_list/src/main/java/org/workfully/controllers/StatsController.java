72e1fc799acd6564299d3001d3be84ed07022d65
Sidi TOURÉ <37058375+sidi001@users.noreply.github.com>
2023-03-13 11:13:49 +0100

    Revert "Revert "Merge pull request #2 from Workfully-github/HE/Frontend/Stats""
    This reverts commit 826e19d586e647021b7b43e06627cd2737600f12.

--- EcommerceFrontend/product_list/src/main/java/org/workfully/controllers/StatsController.java
+++ EcommerceFrontend/product_list/src/main/java/org/workfully/controllers/StatsController.java
@@ -1,10 +1,13 @@
 package org.workfully.controllers;
-
+import org.workfully.http.APIController;
 import org.workfully.models.Stats;
-
 public class StatsController {
+    private APIController apiController;
+    public CategoryController() {
+        this.apiController = new APIController();
+    }
     
-    public Stats getStat(Stats stat) {
-        return stat;
+    public Stats getStat() {
+        return apiController.getStats();
     }
 }
