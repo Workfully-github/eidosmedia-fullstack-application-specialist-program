dcf05e87f34f203c7d4b9432d7bd8a6a50ede85d
Sidi TOURÉ <37058375+sidi001@users.noreply.github.com>
2023-03-15 21:30:40 +0100

    resolve conflit

--- reactapp/src/Components/Cards/Cards.jsx
+++ reactapp/src/Components/Cards/Cards.jsx
@@ -8,22 +8,14 @@ const Cards = (props) => {
   const params = useParams()
 
   const [productList, setProductList] = useState(null);
-<<<<<<< Updated upstream
-  const [searchQuery, setSearchQuery] = useState(params.searchQuery);
-  const [url, setUrl] = useState("https://eidos-api.herokuapp.com/products");
-=======
   //const [searchQuery, setSearchQuery] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
 
   const [url, setUrl] = useState("https://eidos-api.herokuapp.com/api/v1/products");
 
->>>>>>> Stashed changes
   const getProductList = async (url) => {
     // const url = params.searchQuery ? "https://dummyjson.com/products/search?q=" + searchQuery : "https://eidos-api.herokuapp.com/products"
     const response = await axios.get(url);
-<<<<<<< Updated upstream
-    setProductList(response.data.products);
-=======
     if(props.filterType==="stock") {
       //filter the list of products before set the state here
       var products = response.data.products.filter(p => {return p.stock==props.stockQuery})
@@ -36,14 +28,11 @@ const Cards = (props) => {
 
       setIsLoading(false)
     }, 500)
->>>>>>> Stashed changes
   }
 
   const skeletonArr = ["I am", "gonna", "impliment", "this", "skeleton", "loader", "later", "in", "another", "way", "trust", "me"];
 
   useEffect(() => {
-<<<<<<< Updated upstream
-=======
     if(props.filterType==="stock"){
       //the filter will be done in the getProductsList()
       setUrl("https://eidos-api.herokuapp.com/api/v1/products")
@@ -57,7 +46,6 @@ const Cards = (props) => {
       console.log("never be can be here")
       setUrl(props.searchValue ? "https://dummyjson.com/products/search?q=" + props.searchValue : "https://eidos-api.herokuapp.com/api/v1/products")
     }
->>>>>>> Stashed changes
     getProductList(url);
   }, [url])
 
