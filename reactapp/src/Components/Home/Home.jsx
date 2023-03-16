1ed77b4032098de1cb7df9976b1b601264bfb00a
Abderraouf Rahmani <abderraouf.dev@gmail.com>
2023-03-16 16:57:32 +0100

    UI Refactoring

--- reactapp/src/Components/Home/Home.jsx
+++ reactapp/src/Components/Home/Home.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 
 import axios from 'axios'
-import { Container} from "reactstrap";
+import { Container } from "reactstrap";
 import "./Home.css";
 import Navbar from "../Navbar/Navbar";
 import SearchComponent from '../SearchComponent/SearchComponent';
@@ -10,7 +10,7 @@ import Cards from '../Cards/Cards';
 import Pagination from "../Pagination/Pagination"
 import Footer from '../Footer/Footer';
 
-import {Box, Drawer, IconButton, FormControl, Select, MenuItem, FormControlLabel, InputLabel} from '@mui/material';
+import { Box, Drawer, IconButton, FormControl, Select, MenuItem, FormControlLabel, InputLabel } from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
 
 
@@ -19,8 +19,8 @@ import { useStateContext } from "../Context/StateContext"
 
 
 export default function Home(props) {
- //the idea will be to get the search by the url
-  var {searchQuery} = useParams();
+  //the idea will be to get the search by the url
+  var { searchQuery } = useParams();
   var [isDrawerOpen, setIsDrawerOpen] = useState(false);
   var [isLoading, setIsLoading] = useState(true);
   var [categoriesList, setCategoriesList] = useState(null);
@@ -28,53 +28,58 @@ export default function Home(props) {
   var [stock, setStock] = useState(null);
   var [selectMessage, setSelectMessage] = useState(false);
   var [searchValue, setSearchValue] = useState(searchQuery);
-  const { url,setUrl, filterType, setFilterType } = useStateContext();
+  const { url, setUrl, filterType, setFilterType } = useStateContext();
 
-  useEffect(()=>{
+  useEffect(() => {
     getCategoriesList("https://eidos-api.herokuapp.com/api/v1/categories")
-    {console.log(searchQuery)}
+    { console.log(searchQuery) }
   }, [])
 
   const getCategoriesList = async (url) => {
-        setIsLoading(true)
-        const response = await axios.get(url);
-        setCategoriesList(response.data);
-        setTimeout(() => {
-        setIsLoading(false)
-        }, 500)
-    }
+    setIsLoading(true)
+    const response = await axios.get(url);
+    setCategoriesList(response.data);
+    setTimeout(() => {
+      setIsLoading(false)
+    }, 500)
+  }
 
 
   return (
     <>
       <Navbar />
-      <IconButton size="large" edge="start" color="inherit" aria-label='logo' onClick={()=>setIsDrawerOpen(true)}>
-         <MenuIcon/>
+      <IconButton size="large" edge="start" color="inherit" aria-label='logo' onClick={() => setIsDrawerOpen(true)}>
+        <MenuIcon />
       </IconButton>
-      <Drawer anchor='left' open={isDrawerOpen} onClose={()=>setIsDrawerOpen(false)}>
+      <Drawer anchor='left' open={isDrawerOpen} onClose={() => setIsDrawerOpen(false)}>
         <Box p={2} width="250px" textAlign='center' role='presentation' >
-            <h4>Filter:</h4>
-            <br/>
-            <FormControl fullWidth>
-                <InputLabel id="menu">Categories</InputLabel>
-                <Select labelId="menu" id="menu-list" label="categories">
-                    {!isLoading && categoriesList.map(cat => 
-                    <MenuItem value={cat} onClick={()=>{
-                        setUrl("https://dummyjson.com/products/category/" + cat)
-                        setCategory(cat)
-                        // setIsDrawerOpen(false)
-                        setSelectMessage(true)
-                        setSearchValue("")
-                        
-                    }}>
-                        {cat}
-                    </MenuItem>)}
-                </Select>
-            </FormControl>
+          <h4>Filter:</h4>
+          <br />
+          <FormControl fullWidth>
+            <InputLabel id="menu">Categories</InputLabel>
+            <Select labelId="menu" id="menu-list" label="categories">
+              {!isLoading && categoriesList.map(cat =>
+                <MenuItem value={cat} onClick={() => {
+                  setUrl("https://dummyjson.com/products/category/" + cat)
+                  setCategory(cat)
+                  // setIsDrawerOpen(false)
+                  setSelectMessage(true)
+                  setSearchValue("")
+
+                }}>
+                  {cat}
+                </MenuItem>)}
+            </Select>
+          </FormControl>
         </Box>
       </Drawer>
       <SearchComponent val={searchValue} />
-      {selectMessage && <div>filter for Category/stock : {category}</div>}
+      {selectMessage &&
+      <div className="container">
+
+      <div>filter for Category/stock : <b >{category}</b></div>
+      </div>
+      }
       <Container >
 
         <Cards />
