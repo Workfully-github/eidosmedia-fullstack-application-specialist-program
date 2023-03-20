9fd50f6d5801b9cb007f6707772c44e1145831cc
Ricardo Mendes <ricardominamendes@outlook.com>
2023-03-20 23:35:06 +0000

    Local Caching Implementation, Needs Refactor

--- src/main/java/org/workfully/servlets/api/v2/resources/Products.java
+++ src/main/java/org/workfully/servlets/api/v2/resources/Products.java
@@ -1,7 +1,6 @@
 package org.workfully.servlets.api.v2.resources;
 
 import java.io.File;
-
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
@@ -9,7 +8,6 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-
 import org.workfully.utils.MakeFileUtils;
 import org.workfully.utils.RestUtils;
 
@@ -18,6 +16,9 @@ public class Products {
 
     private RestUtils rest = new RestUtils();
     private final static String RESOURCES_BASE_PATH = "src/main/java/org/workfully/resources/";
+    private final static String HEROKU_RESOURCES_BASE_PATH = System.getenv("RESOURCES_BASE_PATH");
+    
+    private static String getAllProductsResponse = null;
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
@@ -28,7 +29,7 @@ public class Products {
             String response = rest.getBody(request).getEntity().toString();
             File file = MakeFileUtils.makeFile(response, GET_PRODUCTS_PATH);
             String localResponse = MakeFileUtils.readFile(file);
-            
+
             rest.updateStats("page");
             return Response.ok()
                     .entity(localResponse)
@@ -64,9 +65,6 @@ public class Products {
                     .build();
         } catch (Exception e) {
             return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(e.getMessage()).build();
-        } finally {
-            new File(RESOURCES_BASE_PATH + "temp_" + PRODUCT_ID_PATH + ".json").delete();
-            System.out.println("Temp file deleted");
         }
     }
 
@@ -117,15 +115,12 @@ public class Products {
     @Path("/allproducts")
     @Produces(MediaType.APPLICATION_JSON)
     public Response getAllProducts() {
-        final String ALL_PRODUCTS_PATH = "allproducts";
-
+        final String ALL_PRODUCTS = "allproducts";
+        String request = "https://dummyjson.com/products?limit=100";
         try {
-            String request = "https://dummyjson.com/products?limit=100";
-            String response = rest.getBody(request).getEntity().toString();
-            File file = MakeFileUtils.makeFile(response, ALL_PRODUCTS_PATH);
+            callApiIfResponseNull(Products.getAllProductsResponse, request);
+            File file = MakeFileUtils.makeFile(getAllProductsResponse, ALL_PRODUCTS);
             String localResponse = MakeFileUtils.readFile(file);
-
-
             rest.updateStats("products");
             return Response.ok()
                     .entity(localResponse)
@@ -137,5 +132,28 @@ public class Products {
         } catch (Exception e) {
             return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(e.getMessage()).build();
         }
+    }
+
+    public static void main(String[] args) {
+        Products products = new Products();
+        products.getAllProducts();
+    }
+
+    public void callApiIfResponseNull(String response, String request) {
+        if (response == null) {
+            getAllProductsResponse = rest.getBody(request).getEntity().toString();
+        }
+    }
+
+    public void cronJobIfApiResponseDifferent(String response, String request) {
+        if (response.hashCode() != rest.getBody(request).getEntity().toString().hashCode()) {
+            getAllProductsResponse = rest.getBody(request).getEntity().toString();
+            System.out.println("Api called, Hash Code is different...");
+        }
+    }
+
+    public void cronJob() {
+        final String REQUEST_ALL_PRODUCTS = "https://dummyjson.com/products?limit=100";
+        cronJobIfApiResponseDifferent(Products.getAllProductsResponse, REQUEST_ALL_PRODUCTS);
     }
 }
--- src/main/java/org/workfully/utils/MakeFileUtils.java
+++ src/main/java/org/workfully/utils/MakeFileUtils.java
@@ -10,11 +10,12 @@ import com.google.gson.GsonBuilder;
 
 public class MakeFileUtils {
 
-    private final static String RESOURCES_BASE_PATH = "src/main/java/org/workfully/resources/";
+    private final static String RESOURCES_BASE_PATH = "./src/main/java/org/workfully/resources/";
+    private final static String HEROKU_RESOURCES_BASE_PATH = System.getenv("RESOURCES_BASE_PATH");
 
     private static File makeJsonFile(String data, String jsonName) throws IOException {
 
-        File file = new File(RESOURCES_BASE_PATH + jsonName + ".json");
+        File file = new File(HEROKU_RESOURCES_BASE_PATH + jsonName + ".json");
 
         if (!file.exists()) {
             System.out.println(jsonName + " created");
@@ -27,24 +28,36 @@ public class MakeFileUtils {
         return file;
     }
 
-    private static File makeTempFile(String data, String jsonName) {
-        try {
-            File file = new File(RESOURCES_BASE_PATH + "temp_" + jsonName + ".json");
-            FileWriter writer = new FileWriter(file);
-            writer.write(formatJson(data));
-            writer.close();
-            System.out.println(jsonName + " temp created");
-            return file;
-        } catch (Exception e) {
-            System.out.println("Unable to create tempfile");
-            e.printStackTrace();
+    private static File makeTempFile(String data, String fileName) {
+        String tempDir = HEROKU_RESOURCES_BASE_PATH;
+        if (tempDir == null) {
+            System.err.println(tempDir + " environment variable not set");
             return null;
         }
 
+        File dir = new File(tempDir);
+        if (!dir.exists()) {
+            if (!dir.mkdirs()) {
+                System.err.println("Failed to create temp directory: " + dir.getAbsolutePath());
+                return null;
+            }
+        }
+
+        File file = new File(dir, fileName + ".json");
+        try (FileWriter writer = new FileWriter(file)) {
+            writer.write(formatJson(data));
+            System.out.println(file.getAbsolutePath() + " created");
+            return file;
+        } catch (IOException e) {
+            System.err.println("Failed to write to temp file: " + file.getAbsolutePath());
+            e.printStackTrace();
+            return null;
+        }
     }
 
     private static void mergeFiles(File file, File tempFile) throws IOException {
         file = tempFile;
+        tempFile.delete();
     }
 
     public static String readFile(String filePath) throws IOException {
@@ -55,12 +68,18 @@ public class MakeFileUtils {
         return gson.toJson(jsonObject);
     }
 
-    public static String readFile(File file) throws IOException {
+    public static String readFile(File file) {
+
         Gson gson = new Gson();
-        BufferedReader reader = new BufferedReader(new FileReader(file));
-        Object jsonObject = gson.fromJson(reader, Object.class);
-        reader.close();
-        return gson.toJson(jsonObject);
+        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+
+            Object jsonObject = gson.fromJson(reader, Object.class);
+            return gson.toJson(jsonObject);
+        } catch (Exception e) {
+            System.out.println("Unable to read " + file.getName());
+            e.printStackTrace();
+            return null;
+        }
     }
 
     private static String formatJson(String jsonString) {
@@ -68,10 +87,10 @@ public class MakeFileUtils {
         return gson.toJson(gson.fromJson(jsonString, Object.class));
     }
 
-    public static File makeFile(String response, String path) {
+    public static File makeFile(String response, String pathResource) {
         try {
-            File tempFile = MakeFileUtils.makeTempFile(response, path);
-            File file = MakeFileUtils.makeJsonFile(response, path);
+            File tempFile = MakeFileUtils.makeTempFile(response, pathResource);
+            File file = MakeFileUtils.makeJsonFile(response, pathResource);
 
             if (file.hashCode() != tempFile.hashCode())
                 MakeFileUtils.mergeFiles(file, tempFile);
@@ -82,13 +101,13 @@ public class MakeFileUtils {
             return null;
         } /*
            * finally {
-           * new File(RESOURCES_BASE_PATH + "temp_" + path + ".json").delete();
+           * new File(RESOURCES_BASE_PATH + "temp_" + pathResource + ".json").delete();
            * System.out.println("Temp file deleted");
            * }
            */
     }
 
     public static void main(String[] args) {
-        makeTempFile("Test", "products_id_1");
+        makeTempFile("Test", "allproducts");
     }
 }
